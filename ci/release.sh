014417fe9c6f02e5e4ff221a85b67fd58383e8d3
Tomasz Maczukin <tomasz@maczukin.pl>
2016-09-12 19:01:49 +0200

    Add styles for index file

--- ci/release.sh
+++ ci/release.sh
@@ -63,6 +63,20 @@ __prepare_index() {
     <head>
         <meta charset="utf-8/">
         <title>${title}</title>
+        <style type="text/css">
+            body {font-family: arial, serif; font-size: 14px; margin: 40px; padding: 0;}
+            h1 {border-bottom: 1px solid #aaa; padding: 10px;}
+            p {font-size: 0.85em; margin: 5px 10px;}
+            p span {display: inline-block; font-weight: bold; width: 75px;}
+            p a {color: #000; font-weight: bold; text-decoration: none;}
+            p a:hover {text-decoration: underline;}
+            ul {background: #eee; border: 1px solid #aaa; border-radius: 3px; box-shadow: 0 0 5px #aaa inset; list-style-type: none; margin: 10px; padding: 20px;}
+            li {margin: 5px 0; padding: 2px;}
+            li:hover {background: #dedede;}
+            li a {color: #000; text-decoration: none;}
+            .file_name {display: inline-block; font-weight: bold; width: calc(100% - 100px)}
+            .file_size {display: inline-block; width: 90px;}
+        </style>
     </head>
     <body>
         <h1>${title}</h1>
@@ -73,13 +87,13 @@ EOF
     for file in ${files}; do
         size=$(du -k ${file} | awk '{printf "%0.2f", $1/1024}')
         echo -e "found \033[32m${file}\033[36m [${size} MiB]\033[0m"
-        echo "            <li><a href=\"./${file}\"><span class=\"file_name\">${file}</span></a> <span class=\"file_size\">${size} MiB</span></li>" >> ${index_file}
+        echo "            <li><a href=\"./${file}\"><span class=\"file_name\">${file}</span> <span class=\"file_size\">${size} MiB</span></a></li>" >> ${index_file}
     done
 
     cat >> ${index_file} <<EOF
         </ul>
-        <p>Sources: <a href="${sources_url}">${sources_url}</a></p>
-        <p>Created at: ${CREATED_AT}</p>
+        <p><span>Sources:</span> <a href="${sources_url}">${sources_url}</a></p>
+        <p><span>Created at:</span> ${CREATED_AT}</p>
     </body>
 </html>
 EOF
